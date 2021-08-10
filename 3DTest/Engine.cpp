e1d697f39c73ae5438ae8af48c87c0c9cdcea05d
Christopher K <sejancolach@gmail.com>
2021-08-10 19:44:21 +0200

    Engine.cpp Refactor

--- 3DTest/Engine.cpp
+++ 3DTest/Engine.cpp
@@ -71,129 +71,18 @@ Engine::Engine(int width, int height) {
 
 void Engine::MainLoop(void) { 
     SceneManager sceneManager;
+    util::Shader::Init();
 
     GLuint VertexArrayID;
     glGenVertexArrays(1, &VertexArrayID);
     glBindVertexArray(VertexArrayID);
-    static const GLfloat g_vertex_buffer_data[] = {
-    -1.0f,-1.0f,-1.0f, // triangle 1 : begin
-    -1.0f,-1.0f, 1.0f,
-    -1.0f, 1.0f, 1.0f, // triangle 1 : end
-    1.0f, 1.0f,-1.0f, // triangle 2 : begin
-    -1.0f,-1.0f,-1.0f,
-    -1.0f, 1.0f,-1.0f, // triangle 2 : end
-    1.0f,-1.0f, 1.0f,
-    -1.0f,-1.0f,-1.0f,
-    1.0f,-1.0f,-1.0f,
-    1.0f, 1.0f,-1.0f,
-    1.0f,-1.0f,-1.0f,
-    -1.0f,-1.0f,-1.0f,
-    -1.0f,-1.0f,-1.0f,
-    -1.0f, 1.0f, 1.0f,
-    -1.0f, 1.0f,-1.0f,
-    1.0f,-1.0f, 1.0f,
-    -1.0f,-1.0f, 1.0f,
-    -1.0f,-1.0f,-1.0f,
-    -1.0f, 1.0f, 1.0f,
-    -1.0f,-1.0f, 1.0f,
-    1.0f,-1.0f, 1.0f,
-    1.0f, 1.0f, 1.0f,
-    1.0f,-1.0f,-1.0f,
-    1.0f, 1.0f,-1.0f,
-    1.0f,-1.0f,-1.0f,
-    1.0f, 1.0f, 1.0f,
-    1.0f,-1.0f, 1.0f,
-    1.0f, 1.0f, 1.0f,
-    1.0f, 1.0f,-1.0f,
-    -1.0f, 1.0f,-1.0f,
-    1.0f, 1.0f, 1.0f,
-    -1.0f, 1.0f,-1.0f,
-    -1.0f, 1.0f, 1.0f,
-    1.0f, 1.0f, 1.0f,
-    -1.0f, 1.0f, 1.0f,
-    1.0f,-1.0f, 1.0f
-    };
 
-    static const GLfloat g_color_buffer_data[] = {
-    0.583f,  0.771f,  0.014f,
-    0.609f,  0.115f,  0.436f,
-    0.327f,  0.483f,  0.844f,
-    0.822f,  0.569f,  0.201f,
-    0.435f,  0.602f,  0.223f,
-    0.310f,  0.747f,  0.185f,
-    0.597f,  0.770f,  0.761f,
-    0.559f,  0.436f,  0.730f,
-    0.359f,  0.583f,  0.152f,
-    0.483f,  0.596f,  0.789f,
-    0.559f,  0.861f,  0.639f,
-    0.195f,  0.548f,  0.859f,
-    0.014f,  0.184f,  0.576f,
-    0.771f,  0.328f,  0.970f,
-    0.406f,  0.615f,  0.116f,
-    0.676f,  0.977f,  0.133f,
-    0.971f,  0.572f,  0.833f,
-    0.140f,  0.616f,  0.489f,
-    0.997f,  0.513f,  0.064f,
-    0.945f,  0.719f,  0.592f,
-    0.543f,  0.021f,  0.978f,
-    0.279f,  0.317f,  0.505f,
-    0.167f,  0.620f,  0.077f,
-    0.347f,  0.857f,  0.137f,
-    0.055f,  0.953f,  0.042f,
-    0.714f,  0.505f,  0.345f,
-    0.783f,  0.290f,  0.734f,
-    0.722f,  0.645f,  0.174f,
-    0.302f,  0.455f,  0.848f,
-    0.225f,  0.587f,  0.040f,
-    0.517f,  0.713f,  0.338f,
-    0.053f,  0.959f,  0.120f,
-    0.393f,  0.621f,  0.362f,
-    0.673f,  0.211f,  0.457f,
-    0.820f,  0.883f,  0.371f,
-    0.982f,  0.099f,  0.879f
-    };
-
-    static const GLfloat g_ground_vBuffer[] = {
-        -25.0f,-10.0f,-25.0f, 
-        -25.0f,-10.0f, 25.0f,
-         25.0f,-10.0f,-25.0f, 
-        -25.0f,-10.0f, 25.0f, 
-         25.0f,-10.0f, 25.0f,
-         25.0f,-10.0f,-25.0f, 
-    };
-
-    glm::mat4 Projection = glm::perspective(glm::radians(60.0f), (float)width / (float)height, 0.01f, 500.0f);
-    //glm::mat4 Projection = glm::ortho(-10.0f,10.0f,-10.0f,10.0f,0.0f,100.0f); // In world coordinates
-
-    glm::mat4 View = glm::lookAt(
-        glm::vec3(4, 3, -3), // Camera is at (4,3,3), in World Space
-        glm::vec3(0, 0, 0), // and looks at the origin
-        glm::vec3(0, 1, 0)  // Head is up (set to 0,-1,0 to look upside-down)
-    );
-
-    // Model matrix : an identity matrix (model will be at the origin)
-    glm::mat4 Model = glm::mat4(1.0f);
-    // Our ModelViewProjection : multiplication of our 3 matrices
-    glm::mat4 mvp = Projection * View * Model; // Remember, matrix multiplication is the other way around
-    
-    util::Shader::Init();
     GLuint programID = util::Shader::GetDefaultShader();
 
     GLuint MatrixID = glGetUniformLocation(programID, "MVP");
 
-
     GLuint textureID = loadBMP("./textures/Grass.bmp");
     GLuint textureID2 = loadBMP("./textures/Rock.bmp");
-
-    // TODO: Tasks
-    //[x] -> Chunk System
-    //[x] -> Multi Thread (std::future)
-    //[-] -> Optimize Optimize() function
-    //[x] -> Mathf::SmoothOctaveNoise
-    //[x] -> Scence System
-    //[x]   -> auto assign GameObject to current scence 
-    //[x]   -> Render if GameObject is active and has a MeshRenderer
-    // -> ... 
 
     GameObject WorldGeneratorObject;
     WorldGeneration* worldGen = static_cast<WorldGeneration*>(WorldGeneratorObject.AddBehaviour(new WorldGeneration));
@@ -201,9 +90,19 @@ void Engine::MainLoop(void) {
     worldGen->textureID1 = textureID;
     worldGen->textureID2 = textureID2;
     worldGen->GenerateWorld();
-
     WorldGeneratorObject.name = "WGO";
 
+
+    glm::mat4 Projection = glm::perspective(glm::radians(60.0f), (float)width / (float)height, 0.01f, 500.0f);
+    glm::mat4 View = glm::lookAt(
+        glm::vec3(4, 3, -3), // Camera is at (4,3,3), in World Space
+        glm::vec3(0, 0, 0), // and looks at the origin
+        glm::vec3(0, 1, 0)  // Head is up (set to 0,-1,0 to look upside-down)
+    );
+    // Model matrix : an identity matrix
+    glm::mat4 Model = glm::mat4(1.0f);
+    // Our ModelViewProjection : multiplication of our 3 matrices
+    glm::mat4 mvp = Projection * View * Model; // Remember, matrix multiplication is the other way around
     // position
     glm::vec3 position = glm::vec3(0, 0, 5);
     // horizontal angle : toward -Z
@@ -218,7 +117,7 @@ void Engine::MainLoop(void) {
     float deltaTime = .0f;
     int fpscount = 0;
     float currentFPS = 75.0f;
-    printf("1\n");
+
     do {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -256,10 +155,8 @@ void Engine::MainLoop(void) {
         );
 
         glm::mat4 mvp = Projection * View * Model;
-
-        static float sinOf = 0;
-        sinOf += deltaTime;
-        //go.transform->AddPosition(glm::vec3(std::sin(sinOf) * deltaTime * 10, 0, 0));
+        
+        //Update and Render Scene Objects
         sceneManager.Update();
         sceneManager.Render(mvp);
         sceneManager.LateUpdate();
